fda5bd56e4dd39c937b59da626547b2994d6a723
Cameron Button and Kristen Chellis <cameron@>
2018-05-14 10:48:02 -0700

    add hard coded beers

---
+++ src/app/mock-beers.ts
@@ -0,0 +1,11 @@
+import { Beer } from './models/beer.model';
+
+export const BEERS: Beer[] = [
+  new Beer('Superfuzz', 'Elysian Brewing', 'Blood Orange Pale Ale', 6.4, 45, 4.3, 'this is the description', 'this is the notes'),
+  new Beer('Citrus Mistress', 'Hop Valley Brewing', 'IPA', 6.3, 80, 4.3, 'this is the description', 'this is the notes'),
+  new Beer('Black Butte Porter', 'Dechutes Brewing', 'Porter', 5.2, 30, 4.3, 'this is the description', 'this is the notes'),
+  new Beer('Cucumber Crush', '10 Barrel Brewing', 'Sour', 5, 4, 4.3, 'this is the description', 'this is the notes'),
+  new Beer('Pabst Blue Ribbon', 'Pabst Brewing', 'Piss', 4.7, 10, 4.3, 'this is the description', 'this is the notes'),
+  new Beer('Maid Marion', '2 Towns Ciderhouse', 'Marionberry Cider', 6, 0, 4.3, 'this is the description', 'this is the notes'),
+  new Beer('Willamette Valley Pinot Noir', 'Boedecker Cellars', 'Pinot Noir', 14, 0, 4.3, 'this is the description', 'this is the notes')
+];
