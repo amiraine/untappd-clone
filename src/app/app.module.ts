3a35b59dbeaa9c6d649acfd242990f98db53f442
Ami Cooper and Kayl Eubanks <kayl@>
2018-05-14 13:39:01 -0700

    instantiate discover and my page components, fill html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,11 @@ import { TimelineComponent } from './timeline/timeline.component';
 import { UserModuleComponent } from './user-module/user-module.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { HomeComponent } from './home/home.component';
-
+import { BreweryPageComponent } from './brewery-page/brewery-page.component';
+import { MyPageComponent } from './my-page/my-page.component';
+import {routing} from './app.routing';
+import { DiscoverComponent } from './discover/discover.component';
+import { BarListComponent } from './bar-list/bar-list.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +19,15 @@ import { HomeComponent } from './home/home.component';
     TimelineComponent,
     UserModuleComponent,
     WishlistComponent,
-    HomeComponent
+    HomeComponent,
+    BreweryPageComponent,
+    MyPageComponent,
+    DiscoverComponent,
+    BarListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
