f389705b9328b0b30bc9d72035e748101aea6216
Ami Cooper and Kayl Eubanks <ami+kayl@>
2018-05-14 14:10:12 -0700

    fix imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-=======
+
 import { HttpModule } from '@angular/http';
-import { routing } from './app.routing';
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularFire2';
 import { AngularFireDatabaseModule } from 'angularFire2/database';
 import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
->>>>>>> 2c26632ae10aa0d7ab82223bd60b8394788a91fc
 
 import { AppComponent } from './app.component';
 import { TimelineComponent } from './timeline/timeline.component';
@@ -21,13 +18,14 @@ import { MyPageComponent } from './my-page/my-page.component';
 import { DiscoverComponent } from './discover/discover.component';
 import { BarListComponent } from './bar-list/bar-list.component';
 
+import { routing } from './app.routing';
+
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
   authDomain: masterFirebaseConfig.authDomain,
   databaseURL: masterFirebaseConfig.databaseURL,
   storageBucket: masterFirebaseConfig.storageBucket
 };
-import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
