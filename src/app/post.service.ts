aaf0e50dba484296cbf9c7f69ecf4f13fe7bb713
Cameron Button and Kristen Chellis <kristen@>
2018-05-14 15:39:18 -0700

    add post section in firebase and add service to handle it

---
+++ src/app/post.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Post } from './models/post.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class PostService {
+  postList: FirebaseListObservable<any[]>;
+  constructor(private database: AngularFireDatabase) {
+      this.postList = database.list('posts');
+   }
+
+  getPosts() {
+    return this.postList;
+  }
+
+  getPostById(postId) {
+    return this.database.object('posts/' + postId);
+  }
+
+  savePost(newPost: Post) {
+    this.postList.push(newPost);
+  }
+
+  deleteBeer(outPost) {
+    let entryInFirebase = this.getPostById(outPost.$key);
+    entryInFirebase.remove();
+  }
+
+}
