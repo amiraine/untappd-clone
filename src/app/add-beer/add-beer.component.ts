b15bccc868781ae2872ffe8783311cd88130cc86
Cameron Button and Kayl Eubanks <kayl@>
2018-05-15 10:37:13 -0700

    Grab brewery reference for adding new beer

--- src/app/add-beer/add-beer.component.ts
+++ src/app/add-beer/add-beer.component.ts
@@ -1,22 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { BeerService } from '../beer.service';
 import { Beer } from '../models/beer.model';
+import { BreweryService } from '../brewery.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-add-beer',
   templateUrl: './add-beer.component.html',
   styleUrls: ['./add-beer.component.css'],
-  providers: [BeerService]
+  providers: [BeerService, BreweryService]
 })
 export class AddBeerComponent implements OnInit {
+  breweryId;
+  brewery;
 
-  constructor(private beerService: BeerService) { }
+  constructor(private route: ActivatedRoute, private beerService: BeerService, private breweryService: BreweryService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.breweryId = parseInt(urlParameters['id']);
+      console.log(this.breweryId + " brewery id");
+    });
+    this.brewery = this.breweryService.getBreweryById(this.breweryId);
+    console.log(this.brewery + " wtf is this?");
   }
 
-  newBeer(name, brewery, type, abv, ibu, rating, description, notes) {
-    let newBeer: Beer = new Beer (name, brewery, type, abv, ibu, rating, description, notes);
+  newBeer(name, type, abv, ibu, rating, description, notes) {
+    let breweryName = this.brewery.name;
+    let newBeer: Beer = new Beer (name, breweryName, type, abv, ibu, rating, description, notes);
+    console.log(newBeer);
     this.beerService.saveBeer(newBeer);
   }
 }
