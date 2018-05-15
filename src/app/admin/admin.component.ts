dfb2130ee8ea72f4b08cea8e50b718356a9a3559
Cameron Button and Kayl Eubanks <kayl@>
2018-05-15 11:44:57 -0700

    Add functionality to submit a new brewery

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Brewery } from '../models/brewery.model';
+import { BreweryService } from '../brewery.service';
+import { Beer } from '../models/beer.model';
 
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'],
+  providers: [BreweryService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private breweryService: BreweryService) { }
 
   ngOnInit() {
   }
 
+  newBrewery(name, address, city, state, zip) {
+    let beer: Beer = new Beer("", "", "", 0, 0, 0, "", "");
+    let beerList: Beer[] = [beer];
+    let newBrewery: Brewery = new Brewery(name, beerList, city, state, address, zip);
+    this.breweryService.saveBrewery(newBrewery);
+  }
 
 }
