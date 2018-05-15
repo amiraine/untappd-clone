adb423c684eb978e472df9f7abf06ecff82bf3c3
Cameron Button and Kayl Eubanks <kayl@>
2018-05-15 16:53:25 -0700

    make user-module work

--- src/app/user-module/user-module.component.ts
+++ src/app/user-module/user-module.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-module',
   templateUrl: './user-module.component.html',
-  styleUrls: ['./user-module.component.css']
+  styleUrls: ['./user-module.component.css'],
+  providers: [UserService]
 })
 export class UserModuleComponent implements OnInit {
-
-  constructor() { }
+  userList;
+  currentUser;
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getUsers().subscribe(dataLastEmittedFromObserver => {
+      this.userList = dataLastEmittedFromObserver;
+      this.currentUser= this.userList[0];
+    })
+
   }
 
 }
