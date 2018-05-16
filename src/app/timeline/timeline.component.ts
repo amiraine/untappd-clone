d215c2cdc8abab76e67819e1fe5d7ecc5baf4459
ami cooper <amiraine.cooper@gmailcom>
2018-05-16 09:45:10 -0700

    css, html

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { PostService } from '../post.service';
+import { BeerService } from '../beer.service';
 
 @Component({
   selector: 'app-timeline',
@@ -6,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.css']
 })
 export class TimelineComponent implements OnInit {
-
+  userList;
+  currentUser;
+  postList;
+  userPostList: Array<object> = new Array();
+  beerList;
   constructor() { }
 
   ngOnInit() {
